ecacb529d7dde7b43deeae3ecca7a5b7b4b7702c
Zero-Ya <ketyanuk111@gmail.com>
2025-06-28 20:45:43 +0700

    Created and visualized array of random numbers

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,24 @@
+import { useEffect, useState } from "react"
+import { randomInts } from "./utils/randomInts.ts"
+
 const App = () => {
+  const [array, setArray] = useState<number[]>([]);
+
+  useEffect(() => {
+    for (let i = 0; i < 200; i++) {
+      const rand = randomInts(100)
+      console.log(rand)
+      setArray(a => [...a, rand])
+    }
+  }, [])
+
   return (
-    <div>App</div>
+    <div className="flex justify-center items-end w-5/6 absolute m-auto top-1/2 left-1/2 -translate-1/2">
+      {array.map((num) => (
+        <div className="w-1 bg-gray-100 border text-center " style={{height: `${num * 5}px`}}>
+        </div>
+      ))}
+    </div>
   )
 }
 
